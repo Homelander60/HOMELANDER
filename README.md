44190036f0f993ff849227d01fd4d41b56583db2
Homelander60 <ordinarygeto@gmail.com>
2024-02-11 13:03:25 +0200

    𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍

---
+++ README.md
@@ -0,0 +1,49 @@
+# 𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍 - WhatsApp Bot
+
+𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍
+<div align="center">
+<a href="https://git.io/typing-svg"><img src="https://readme-typing-svg.demolab.com?font=Ribeye&size=50&pause=1000&color=F710B1&center=true&width=910&height=100&lines=I'M+𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍;Multi+Divice+Whatsapp+Bot;Coded+By+𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍" alt="Typing SVG" /></a>
+  
+<p align="center">  
+  <a href="𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍">
+    <img alt=𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍 height="300" src="https://telegra.ph/file/5d208562c78e40cbfc1bc.jpg">
+   
+</a> 
+    
+</p>
+<p align="center">
+<a 
+
+## Installation
+
+To use 𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍 as a WhatsApp bot:
+
+1: contact owner! +27687077594 
+2: follow and dm on insta; Mirai.art69
+3: email; ordinarygeto@gmail.com, bhell8552@gmail.com
+4: join gc; https://chat.whatsapp.com/Gei7mdybqG8Hf854QUDIdV
+
+if you can deploy
+#### SETUP
+
+Fork The Repo
+    <br>
+<a href="/fork"><img title="Phoenix-MD" src="https://img.shields.io/badge/FORK Phoenix MD-h?color=black&style=for-the-badge&logo=stackshare"></a>
+
+#### Scan QR Code
+
+<a href="https://replit.com/@ordinarygeto/HOMELANDER?v=1/"><img title="𝙃𝙊𝙈𝙀𝙇𝘼𝙉𝘿𝙀𝙍 Scan Qr" src="https://img.shields.io/badge/SCAN QR CODE 1-h?color=black&style=for-the-badge&logo=msi"></a>
+     <br>
+## Configuration
+
+Customize HOMELANDER's behavior by configuring its settings according to your multimedia requirements. You can adjust its analysis algorithms, conversion parameters, and integration with external multimedia services. Refer to the documentation for detailed instructions on how to configure HOMELANDER for WhatsApp multimedia tasks.
+
+## Contributing
+
+Contributions to HOMELANDER are welcome! If you have ideas for new multimedia features, improvements to existing functionalities, or would like to contribute code, please reach out to the project maintainers through the GitHub repository.
+
+## License
+
+HOMELANDER is licensed under the [MIT License](LICENSE).
+
+---
---
+++ plugins/_menu.js
@@ -0,0 +1,84 @@
+const events = require("../lib/event");
+const { pnix, isPrivate, tiny, serif_B, clockString } = require("../lib");
+const { OWNER_NAME, BOT_NAME } = require("../config");
+const { hostname } = require("os");
+const more = String.fromCharCode(8206);
+const readMore = more.repeat(4001);
+let botStartTime = new Date();
+
+pnix(
+  {
+    pattern: "menu",
+    fromMe: isPrivate,
+    type: "main",
+    desc: "Show All commands",
+  },
+  async (message, match, m) => {
+  message.client.sendMessage(message.jid, { react: { text: "📃", key: m.key } });
+    if (match) {
+      for (let i of events.commands) {
+        if (i.pattern.test(message.prefix + match))
+          message.reply(
+            `\`\`\`Command : ${message.prefix}${match.trim()}
+Description : ${i.desc}\`\`\``
+          );
+      }
+    } else {
+      let { prefix } = message;
+      let [date, time] = new Date()
+        .toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+        .split(",");
+      let currentTime = new Date();
+      let runtime = clockString(currentTime - botStartTime);
+
+      let menu = `╭───🦅 *${BOT_NAME}* 🦅
+🦅 *𝐎𝐰𝐧𝐞𝐫* :  ${OWNER_NAME}
+🦅 *𝐩𝐫𝐞𝐟𝐢𝐱* : ${prefix}
+🦅 *𝐩𝐥𝐚𝐭𝐟𝐨𝐫𝐦* : ${hostname().split("-")[0]}
+🦅 *𝐩𝐥𝐮𝐠𝐢𝐧𝐬* : ${events.commands.length} 
+🦅 *𝐫𝐮𝐧𝐭𝐢𝐦𝐞* : ${runtime} 
+🦅\n`;
+      let cmnd = [];
+      let cmd;
+      let category = [];
+      events.commands.map((command, num) => {
+        if (command.pattern) {
+          cmd = command.pattern
+            .toString()
+            .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
+        }
+
+        if (!command.dontAddCommandList && cmd !== undefined) {
+          let type;
+          if (!command.type) {
+            type = "misc";
+          } else {
+            type = command.type.toLowerCase();
+          }
+
+          cmnd.push({ cmd, type: type });
+
+          if (!category.includes(type)) category.push(type);
+        }
+      });
+      cmnd.sort();
+      category.sort().forEach((cmmd) => {
+        menu += `╭───🦅 *${cmmd}* 🦅`;
+        let comad = cmnd.filter(({ type }) => type == cmmd);
+        comad.forEach(({ cmd }, num) => {
+          menu += `\n│  ${cmd.trim()}`;
+        });
+        menu += `\n╰─────────────⦁\n`;
+      });
+
+      menu += ``;
+      return await message.client.sendMessage(message.jid, {
+        image: { url: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIhEw_NHyjOINwHv1HhGF8El9x0RFAwt6Vb1eib9uNqQ&s.jpg` },
+        caption: tiny(menu),
+        footer: tiny(
+          `Homelander\nVersion : ${require("../package.json").version}`
+        ),
+      });
+    }
+  }
+);
